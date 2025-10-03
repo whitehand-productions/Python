15bf1b1435cb1a7255c3e4126cedfa8f9bd7a67b
whitehand-productions <blackhand0404@gmail.com>
2025-10-03 16:28:40 -0700

    vent.py

--- vent.py
+++ vent.py
@@ -2,15 +2,14 @@ import time # Time module
 
 # vent() function is where most of the magic happens
 def vent():
-    line1 = "Everyday when I wake up, I feel like a prisoner of my own soul."
-    line2 = "I look in the mirror, and the displeasure reaches me."
-    line3 = "I just don't want to hate myself any more."
-    line4 = "Self-loathing has created this pyroclastic ash cloud inside of my mind."
-    line5 = "I just want someone to see me, hear my cries, and show me why I should love myself."
-    line6 = "I want to be loved the way I love"
-    line7 = "I just want to be ok."
-
-    lines = [line1, line2, line3, line4, line5, line6, line7] # all 7 lines mashed into paragraph
+    lines = ["Everyday when I wake up, I feel like a prisoner of my own soul.", 
+            "I look in the mirror, and the displeasure reaches me.",
+             "I just don't want to hate myself any more.", 
+             "Self-loathing has created this pyroclastic ash cloud inside of my mind.",
+             "I just want someone to see me, hear my cries, and show me why I should love myself.",
+             "I want to be loved the way I love", 
+             "I just want to be ok."]
+    
 
     for line in lines:
         print(line); time.sleep(1.5) # 1 second and a half pause
@@ -24,4 +23,5 @@ if __name__ == "__main__":
             vent()
         else:
             exit()
+
 
